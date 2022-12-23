d197de654dcb0f11b74d6ac48084c089707ad91e
KotenkoNata <kotenkonata8@gmail.com>
2022-12-23 15:56:51 +0200

    feat: add variables for changing theme

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,26 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+const input = document.querySelector(".theme-switcher__input");
+
+input.addEventListener("change", (e) => {
+    let theme = localStorage.getItem('data-theme');
+    if (theme ==='dark'){
+        changeThemeToLight()
+    }else{
+        changeThemeToDark()
+    }
+});
+
+let theme = localStorage.getItem('data-theme');
+const changeThemeToDark = () => {
+    document.body.setAttribute("data-theme", "dark")
+    localStorage.setItem("data-theme", "dark")
+}
+
+const changeThemeToLight = () => {
+    document.body.setAttribute("data-theme", "light")
+    localStorage.setItem("data-theme", 'light')
+}
+
